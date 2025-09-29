5f03450e200501684946be7b9cd92a36946196fe
Абдуллаев Гамзатхан Гаджиакаевич <10231303@college.local>
2025-09-29 20:16:11 +0300

    Initial commit

---
+++ src/Main.kt
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    val name = "nber"
+    var nber = 123323
+    println("1 число ${nber + 1}")
+    var nber2 = 321133
+    println("2 число ${nber2 + 21}")
+    print("сумма чисел - 22 = ${nber + nber2}")
+
+}
+
+fun sum(a: Int, b: Int): Int {
+    return a + b
+}
